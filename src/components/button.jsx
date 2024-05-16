f15ebd762505779a66b0fc5a5bb6a81b1f6768dc
Byron <byron303@gmail.com>
2024-05-15 18:33:37 -0600

    navbar and header complete

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,16 +1,13 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-block items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${className || ""} ${
-    href ? "cursor-pointer" : ""
-  }`;
-
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
